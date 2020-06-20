743a7396c175a68537189e31c56f6686072efaa8
moriokameda <mkame19941031@gmail.com>
2020-06-21 01:25:03 +0900

    add stateless method

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends Controller
      */
     public function handleProviderCallback(Request $request) {
 
-        $github_user = Socialite::driver('github')->user();
+        $github_user = Socialite::driver('github')->stateless()->user();
         $now = date('y/m/d H:i:s');
         $app_user = DB::select('select * from public.user where github_id = ?', [$github_user->user['login']]);
 
